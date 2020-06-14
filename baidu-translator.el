5fb5e2e6d74027556f2629d0b7ba93f8ca0edf11
lee <loyalpartner@163.com>
2020-06-14 21:07:57 +0800

    fix typo.

--- baidu-translator.el
+++ baidu-translator.el
@@ -74,7 +74,7 @@
     (baidu-translator-translate
      (if (baidu-translator-chinese-p word) "zh" "en")
      (if (baidu-translator-chinese-p word) "en" "zh")
-     (thing-at-point 'sentence-at-point t))))
+     (thing-at-point 'sentence t))))
 
 (with-eval-after-load "evil"
   (evil-define-operator evil-baidu-translate-operator (beg end type)
