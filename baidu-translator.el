618d5046a80107d57b77d1025bf80b50a0c2653b
lee <loyalpartner@163.com>
2020-06-15 12:53:22 +0800

    fix typo.

--- baidu-translator.el
+++ baidu-translator.el
@@ -61,7 +61,7 @@
   (with-current-buffer (get-buffer-create "*baidu translator*")
     (setq buffer-read-only nil)
     (baidu-translator-mode)
-    (visual-line-mode)
+    (visual-line-mode 1)
     (erase-buffer)
     (use-local-map baidu-translator-map)
     (insert (baidu-translator-get-result from to (baidu-translator-trim-tail text)))
