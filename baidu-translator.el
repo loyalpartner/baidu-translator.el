399dd1b247030c1b23c8c6e03713c2a4eefdde95
lee <loyalpartner@163.com>
2020-06-19 08:59:38 +0800

    add gpl.

--- baidu-translator.el
+++ baidu-translator.el
@@ -1,6 +1,30 @@
-;;; -*- lexical-binding: t; -*-
-;; Author:  lee <loyalpartner@163.com>
-;; URL:     https://github.com/loyalpartner/baidu-translator.el
+;;; baidu-translator.el --- baidu translator         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  lee
+
+;; Author: lee;;; -*- lexical-binding: t; -*- <loyalpartner@163.com>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
 
 (defconst baidu-translator--api-host "https://api.fanyi.baidu.com/api/trans/vip/translate")
 
